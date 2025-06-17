0f9c9415d320fe711da668e10aa507ad840cc98b
jamydevelop <127085144+jamydevelop@users.noreply.github.com>
2025-06-17 19:17:30 +0800

    Colors, Fonts and Image

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -2,19 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("my first app"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text("hello ninjas!"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Text("click"),
-        onPressed: () {  },
-      ),
-    ),
+    debugShowCheckedModeBanner: false,
+    home: Home(),
   ));
 }
 
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "my first app",
+          style: TextStyle(
+              color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Image.asset("assets/space-1.webp"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        backgroundColor: Colors.red[600],
+        child: Text(
+          "click",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
